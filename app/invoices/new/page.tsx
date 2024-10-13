498372e8ee42f57e2cced36aadd42612709c2cd8
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-10-13 15:59:29 +0200

    Finalize the form.

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,32 +1,56 @@
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
 
 export default function NewInvoice() {
   return (
-    <main className="flex flex-col justify-center text-center gap-6 max-w-5xl mx-auto my-12">
+    <main className="flex flex-col justify-center gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-semibold">Create a New Invoice</h1>
       </div>
 
-      <form action="/action">
+      <form className="grid gap-4 max-w-xs">
         <div>
-          <label htmlFor="name">Billing Name</label>
-          <input type="text" name="name" title="name" />
+          <Label className="block mb-2 font-semibold text-small" htmlFor="name">
+            Billing Name
+          </Label>
+          <Input type="text" name="name" id="name" />
         </div>
 
         <div>
-          <label htmlFor="email">Billing Email</label>
-          <input type="email" name="email" title="email" />
+          <Label
+            className="block mb-2 font-semibold text-small"
+            htmlFor="email"
+          >
+            Billing Email
+          </Label>
+          <Input type="email" name="email" id="email" />
         </div>
 
         <div>
-          <label htmlFor="value">Value</label>
-          <input type="text" name="value" title="value" />
+          <Label
+            className="block mb-2 font-semibold text-small"
+            htmlFor="value"
+          >
+            Value
+          </Label>
+          <Input type="text" name="value" id="value" />
         </div>
 
         <div>
-          <label htmlFor="value">Description</label>
-          <textarea name="description" title="description"></textarea>
+          <Label
+            className="block mb-2 font-semibold text-small"
+            htmlFor="value"
+          >
+            Description
+          </Label>
+          <Textarea name="description" id="description"></Textarea>
         </div>
+
+        <Button type="submit" className="w-full font-semibold">
+          Submit
+        </Button>
       </form>
     </main>
   );
