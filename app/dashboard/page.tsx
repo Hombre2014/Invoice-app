ade9824cc1a86de603e16e835bbe6532be178186
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-10-13 13:16:36 +0200

    Add a badge component and finish the dashboard-page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import Link from 'next/link';
+import { CirclePlus } from 'lucide-react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 import {
   Table,
   TableBody,
@@ -10,34 +15,44 @@ import {
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center text-center gap-6 max-w-5xl mx-auto">
-      <h1 className="text-5xl font-bold">Dashboard</h1>
+    <main className="flex flex-col justify-center text-center gap-6 max-w-5xl mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-semibold">Invoices</h1>
+        <p>
+          <Button className="inline-flex gap-2" variant="ghost" asChild>
+            <Link href="/invoices/new">
+              <CirclePlus className="h-4 w-4" />
+              Create Invoice
+            </Link>
+          </Button>
+        </p>
+      </div>
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Date</TableHead>
-            <TableHead>Customer</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead className="text-center">Status</TableHead>
-            <TableHead className="text-right">Value</TableHead>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell className="font-medium text-left">
+            <TableCell className="font-medium text-left p-4">
               <span className="font-semibold">10/31/2024</span>
             </TableCell>
-            <TableCell className="text-left">
+            <TableCell className="text-left p-4">
               <span className="font-semibold">Philip J. Fry</span>
             </TableCell>
-            <TableCell className="text-left">
-              <span className="font-semibold">philip@fry.com</span>
+            <TableCell className="text-left p-4">
+              <span className="">philip@fry.com</span>
             </TableCell>
-            <TableCell className="text-center">
-              <span className="font-semibold">Open</span>
+            <TableCell className="text-center p-4">
+              <Badge className="rounded-full">Open</Badge>
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell className="text-right p-4">
               <span className="font-semibold">$250.00</span>
             </TableCell>
           </TableRow>
