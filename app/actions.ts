342c1ce3e36058429680226f53c8d0a8d0b6c632
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-10-23 10:52:32 +0200

    Restricting the access to the dashboard and individual invoices pages to the user created them.

--- app/actions.ts
+++ app/actions.ts
@@ -1,19 +1,26 @@
 'use server';
 
 import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs/server';
 
 import { db } from '@/db';
 import { Invoices } from '@/db/schema';
 
 export async function createInvoice(formData: FormData) {
+  const { userId } = auth();
   const value = Math.floor(parseFloat(String(formData.get('value'))) * 100);
   const description = formData.get('description') as string;
+
+  if (!userId) {
+    return;
+  }
 
   const results = await db
     .insert(Invoices)
     .values({
       value,
       description,
+      userId,
       status: 'open',
     })
     .returning({
