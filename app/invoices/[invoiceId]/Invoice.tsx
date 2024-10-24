79372d3a9aff686a3850c1640e0a46788da2423a
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-10-24 15:56:19 +0200

    Add the delete invoice functionality.

---
+++ app/invoices/[invoiceId]/Invoice.tsx
@@ -0,0 +1,173 @@
+'use client';
+
+import { useOptimistic } from 'react';
+import { ChevronDown, Ellipsis, Trash2 } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+import { Invoices } from '@/db/schema';
+import { Badge } from '@/components/ui/badge';
+import Container from '@/components/Container';
+import { Button } from '@/components/ui/button';
+import { AVAILABLE_STATUSES } from '@/data/invoices';
+import { updateStatusAction, deleteInvoiceAction } from '@/app/actions';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+interface InvoiceProps {
+  invoice: typeof Invoices.$inferSelect;
+}
+
+export default function Invoice({ invoice }: InvoiceProps) {
+  const [currentStatus, setCurrentStatus] = useOptimistic(
+    invoice.status,
+    (state, newStatus) => {
+      return String(newStatus);
+    }
+  );
+
+  async function handleOnUpdateStatus(formData: FormData) {
+    const originalStatus = currentStatus;
+    setCurrentStatus(formData.get('status'));
+
+    try {
+      await updateStatusAction(formData);
+    } catch (e) {
+      setCurrentStatus(originalStatus);
+    }
+  }
+
+  return (
+    <main className="w-full h-full">
+      <Container>
+        <div className="flex justify-between mb-8">
+          <h1 className="flex items-center gap-4 text-3xl font-semibold">
+            Invoice {invoice.id}
+            <Badge
+              className={cn(
+                'rounded-full capitalize',
+                currentStatus === 'open' && 'bg-blue-500',
+                currentStatus === 'paid' && 'bg-green-500',
+                currentStatus === 'void' && 'bg-gray-500',
+                currentStatus === 'uncollectible' && 'bg-red-500'
+              )}
+            >
+              {currentStatus}
+            </Badge>
+          </h1>
+          <div className="flex gap-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="flex items-center gap-2">
+                  Change Status
+                  <ChevronDown className="w-4 h-auto" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {AVAILABLE_STATUSES.map((status) => (
+                  <DropdownMenuItem key={status.id}>
+                    <form action={handleOnUpdateStatus}>
+                      <input type="hidden" name="id" value={invoice.id} />
+                      <input type="hidden" name="status" value={status.id} />
+                      <button type="submit">{status.label}</button>
+                    </form>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <Dialog>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" className="flex items-center gap-2">
+                    <span className="sr-only">More Options</span>
+                    <Ellipsis className="w-4 h-auto" />
+                    <ChevronDown className="w-4 h-auto" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <DialogTrigger asChild>
+                      <button type="submit" className="flex gap-2 items-center">
+                        <Trash2 className="w-4 h-auto" />
+                        Delete Invoice
+                      </button>
+                    </DialogTrigger>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle className="text-2xl">
+                    Delete Invoice?
+                  </DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your invoice and remove your data from our servers.
+                  </DialogDescription>
+                  <DialogFooter className="!flex !justify-center">
+                    <form action={deleteInvoiceAction}>
+                      <input type="hidden" name="id" value={invoice.id} />
+                      <Button
+                        variant="destructive"
+                        type="submit"
+                        className="flex gap-2 items-center"
+                      >
+                        <Trash2 className="w-4 h-auto" />
+                        Delete Invoice
+                      </Button>
+                    </form>
+                  </DialogFooter>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+
+        <p className="text-3xl mb-3">${(invoice.value / 100).toFixed(2)}</p>
+        <p className="text-lg mb-8">{invoice.description}</p>
+        <h2 className="font-bold text-lg bm-4">Billing Details</h2>
+
+        <ul className="grid gap-2">
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice ID
+            </strong>
+            <span>{invoice.id}</span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Date
+            </strong>
+            <span>{new Date(invoice.createTs).toLocaleDateString()}</span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Name
+            </strong>
+            <span></span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Email
+            </strong>
+            <span></span>
+          </li>
+        </ul>
+      </Container>
+    </main>
+  );
+}
